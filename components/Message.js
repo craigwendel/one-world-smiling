cbafc10a9532d05eda24f82c6eda024db9688dd6
Craig Wendel <wendelcraig@gmail.com>
2021-09-21 17:32:23 -0500

    Adjust Message component layout, images

--- components/Message.js
+++ components/Message.js
@@ -4,11 +4,16 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '5rem',
+    [theme.breakpoints.down('xs')]: {
+      padding: '3rem',
+    },
+  },
   message: {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     gridGap: '2rem',
-    padding: '5rem',
     justifyItems: 'center',
     [theme.breakpoints.down('xs')]: {
       gridTemplateColumns: '1fr',
@@ -22,58 +27,32 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  image: {
-    display: 'flex',
-    alignItems: 'center',
-  },
 }));
 
 export default function Message() {
   const classes = useStyles();
   return (
-    <div className={classes.message}>
-      <div className={classes.textImage}>
-        <Typography
-          className={classes.type}
-          color="primary"
-          variant="h3"
-        >{`Jaylen's Message`}</Typography>
-        <div className={classes.image}>
-          <Image
-            src="/jaylen.png"
-            width={350}
-            height={450}
-            alt="hands holding a hand written note"
-          />
-          <Image
-            src="/jaylens-message.png"
-            width={400}
-            height={450}
-            alt="hands holding a hand written note"
-          />
-        </div>
+    <div className={classes.root}>
+      <Typography
+        className={classes.type}
+        color="primary"
+        variant="h3"
+        align="center"
+      >{`Jaylen's Message`}</Typography>
+      <div className={classes.message}>
+        <Image
+          src="/jaylens-message.png"
+          width={400}
+          height={450}
+          alt="hands holding a hand written note"
+        />
+        <Image
+          src="/jaylen.png"
+          width={350}
+          height={400}
+          alt="jaylen school photo"
+        />
       </div>
-      {/* <Typography variant="h3" color="primary" align="center">
-        My Message
-      </Typography>
-      <Typography variant="h6" align="center">
-        The meaning of this product is to spread the smiles. So many people are
-        going through situations that may take away their smiles, for example
-        Covid. So please wear this and spread the smile...we need more smiles in
-        the world.
-      </Typography>
-      <Typography variant="h6" color="primary" align="center">
-        CEO - Jaylen - 9 yrs. old
-      </Typography>
-      <Typography variant="h4" color="secondary" align="center">
-        1 World Smiling
-      </Typography> */}
-      <Image
-        src="/smile-world-text.png"
-        width={300}
-        height={350}
-        alt="one world smiling logo"
-      />
     </div>
   );
 }
